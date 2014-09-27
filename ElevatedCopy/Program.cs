7008c87932e9c0535181a9ca5db6394fe2b88b55
Ryan Farley <ryan@farleyzone.com>
2014-09-26 18:23:57 -0700

    Initial commit

---
+++ ElevatedCopy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ElevatedCopy
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                var process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = "/c COPY \"" + args[0] + "\" \"" + args[1] + "\"";
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.Verb = "runas";
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                process.Start();
+            }
+        }
+    }
+}
